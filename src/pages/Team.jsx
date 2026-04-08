27166255acb38b0cbfb6aae6e481f953266a9d6a
jmgboss1-try <jmgboss1@gmail.com>
2026-04-08 16:03:31 +0900

    Create Team.jsx

---
+++ src/pages/Team.jsx
@@ -0,0 +1,8 @@
+export default function Team() {
+  return (
+    <div>
+      <div style={{fontSize:20,fontWeight:700,marginBottom:8}}>👥 팀원 소개</div>
+      <div style={{fontSize:12,color:'#5e6585'}}>준비 중입니다...</div>
+    </div>
+  )
+}
