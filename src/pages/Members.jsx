d5df94505106a65639909f9eab82dedf083358a7
jmgboss1-try <jmgboss1@gmail.com>
2026-04-08 16:03:05 +0900

    Add Members component with initial layout

---
+++ src/pages/Members.jsx
@@ -0,0 +1,8 @@
+export default function Members() {
+  return (
+    <div>
+      <div style={{fontSize:20,fontWeight:700,marginBottom:8}}>📁 인원관리</div>
+      <div style={{fontSize:12,color:'#5e6585'}}>준비 중입니다...</div>
+    </div>
+  )
+}
